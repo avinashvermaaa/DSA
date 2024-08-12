bf7926a0b06c8564f9b921bf123451a15ae5dd80
Avinash verma <105039208+avinashvermaaa@users.noreply.github.com>
2024-08-12 21:18:37 +0530

    Update stl.cpp

--- STL/Vector/stl.cpp
+++ STL/Vector/stl.cpp
@@ -12,3 +12,11 @@ int sum = accumulate(nums.begin(),nums.end(),0);      // 0 as initial value of s
 
 // Reverse vector
   reverse(nums.begin(),nums.end());                  // 12345  --> 54321
+
+// Erase a number or a specific range of number
+  nums.erase(nums.begin() + 2);      nums.erase(nums.begin());    // erase the element at index 2 {0,1, [2] }   and erase the element at index 0.
+  nums.erase(nums.begin()+1, nums.begin()+4 )                     // erase [{4-1} = 3] elements starting from 1st index.
+  nums.erase(remove(nums.begin(),nums.end(),0),nums.end());       // (erase / remove) all occurenece of 0.
+          OR  
+  nums.erase(remove(begin(nums),end(nums),0), end(nums));         // (erase / remove) all occurenece of 0.
+  
